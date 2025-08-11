dd4297dd2e6c22bf22dee58c0b3b6b5a9ef0c6bb
Harry_Dao <harry.dev.1999@gmail.com>
2025-08-11 14:20:35 +0700

    commit lan 2

--- C#Backend/Program.cs
+++ C#Backend/Program.cs
@@ -10,6 +10,8 @@ namespace C_Backend
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a C# backend application."); 
         }
     }
 }
